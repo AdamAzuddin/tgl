f3c9650726392b2e5c2a133c25587005f255b9d1
Adam <posable-intern42@hotmail.com>
2025-08-14 15:22:58 +0800

    added basic commands

--- main.go
+++ main.go
@@ -1,8 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Welcome to ToGoList")
+
+	if len(os.Args) < 2 {
+		showHelp()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		handleAdd()
+	case "list":
+		handleList()
+	case "tick":
+		handleTick()
+	case "rmv":
+		handleRmv()
+	case "help", "-h", "--help":
+		showHelp()
+
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		showHelp()
+	}
+
 }
 
+func handleAdd() {
+	fmt.Println("Handle Add")
+}
+
+func handleList() {
+	fmt.Println("Handle List")
+}
+
+func handleTick() {
+	fmt.Println("Handle Tick")
+}
+
+func handleRmv() {
+	fmt.Println("Handle Remove")
+}
+
+func showHelp() {
+	fmt.Println("tgl - A simple todo list CLI")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("  tgl add \"task description\"  - Add a new task")
+	fmt.Println("  tgl list                    - List all tasks")
+	fmt.Println("  tgl tick [id]               - Mark task as completed")
+	fmt.Println("  tgl rmv [id]                - Remove a task")
+	fmt.Println("  tgl help                    - Show this help")
+	fmt.Println("")
+	fmt.Println("Examples:")
+	fmt.Println("  tgl add \"Buy groceries\"")
+	fmt.Println("  tgl list")
+	fmt.Println("  tgl tick 1")
+	fmt.Println("  tgl rmv 2")
+}
